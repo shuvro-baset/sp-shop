1d71dfa24ee86dbb9fda0533551ae4c7e7530e11
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-30 00:51:28 +0600

    get product into client side

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import {  Container, Row } from 'react-bootstrap';
 import {categories} from '../../data';
 import Product from '../Product/Product';
 
-const Products = () => {
+const Products = ({cat, filters, sort}) => {
+    console.log(cat, filters, sort);
+    const [products, setProducts] = useState([]);
+    const [filterProducts, setFilterProducts] = useState([]);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const res = await axios.get("http://localhost:5000/api/products")
+            } catch (err) {}
+        };
+    }, [cat])
     return (
         <Container>
             <Row>
