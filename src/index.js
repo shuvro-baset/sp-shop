9e2404b5467950d3c5f093b15bd1eb08dca49f3f
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-31 12:06:58 +0600

    s

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { reducers } from './reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
